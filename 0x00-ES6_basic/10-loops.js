180d2d2dcd7a5c85d75ff3d15bba6c52f607f089
ssun97 <sunmingyu.97@gmail.com>
2021-07-13 07:04:38 +0000

    first push

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const newArray = [];
+  for (const idx of array) {
+    const value = appendString + idx;
+    newArray.push(value);
+  }
+  return newArray;
+}
