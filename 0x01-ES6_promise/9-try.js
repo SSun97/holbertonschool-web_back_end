be9ec81913d549f04c6ce65226137d99321aebaf
ssun97 <sunmingyu.97@gmail.com>
2021-07-16 02:18:23 +0000

    first push

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,12 +1,7 @@
 export default function guardrail(mathFunction) {
-  const queue = [];
   try {
-    const val = mathFunction();
-    queue.push(val);
-  } catch (error) {
-    queue.push(error);
-  } finally {
-    queue.push('Guardrail was processed');
+    return [mathFunction(), 'Guardrail was processed'];
+  } catch (value) {
+    return [`${value.name}: ${value.message}`, 'Guardrail was processed'];
   }
-  return queue;
 }
