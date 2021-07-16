2a1787da6a2bd575b12b5c194e1e851ef047de35
ssun97 <sunmingyu.97@gmail.com>
2021-07-16 02:00:39 +0000

    first push

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,10 +1,13 @@
+/* eslint-disable no-param-reassign */
+
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled(signUpUser(firstName, lastName), uploadPhoto(fileName))
-    .then((results) => results.map((result) => ({
-      status: result.status,
-      value: result.value,
-    })));
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((result) => {
+      result[1].value = `Error: ${result[1].reason.message}`;
+      delete result[1].reason;
+      return result;
+    });
 }
