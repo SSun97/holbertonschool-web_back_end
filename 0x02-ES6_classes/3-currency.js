797f59a0f4898fd4a0513ae3109461e6cc079040
Simon Sun <sunmingyu.97@gmail.com>
2021-07-19 22:17:50 -0500

    first push

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,22 @@
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+  set code(newVal){
+    if (typeof newVal !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    this._code = newVal;
+  }
+
+  set name(newVal){
+    if (typeof newVal !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = newVal;
+  }
+  displayFullCurrency() {
+    console.log(this._name + ' (' + this._code + ')');
+  }
+}
