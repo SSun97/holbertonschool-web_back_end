8de1946b43d638a29b7b622bfa5947ba4f20f799
Simon Sun <sunmingyu.97@gmail.com>
2021-07-19 22:47:26 -0500

    first push

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,35 @@
+import Currency from "./3-currency";
+
+export default class Pricing{
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newVal) {
+    if (typeof newVal !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    this._amount = newVal;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(newVal) {
+    this._currency = newVal;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency}`
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
