31235aeadd7876b977cd27ebcff286c8e7601218
ssun97 <sunmingyu.97@gmail.com>
2021-07-21 22:58:35 +0000

    first push

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,5 +1,6 @@
 // https://www.youtube.com/watch?v=UYkJaW3pmj0
 export default function createInt8TypedArray(length, position, value) {
+  if (position > length) throw new Error('Position outside range');
   const buffer = new ArrayBuffer(length);
   const dv1 = new DataView(buffer);
   dv1.setInt8(position, value);
