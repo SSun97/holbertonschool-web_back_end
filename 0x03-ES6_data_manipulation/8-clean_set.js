ab87af8ed7977f22ec9fb942a81f1850e329acf3
ssun97 <sunmingyu.97@gmail.com>
2021-07-22 00:23:37 +0000

    first push

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 export default function cleanSet(set, startString) {
   if (startString === '') return '';
-  const array = [...set];
+  const array = [...set].filter((el) => el);
   const newArray = array.filter((item) => item.startsWith(startString));
   const arrayAfterCut = newArray.map((item) => item.substring(startString.length));
   const string = arrayAfterCut.join('-');
