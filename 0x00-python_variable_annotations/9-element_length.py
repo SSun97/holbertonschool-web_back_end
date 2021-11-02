a0947f19bff8795aaa8e4f2d2f7f0e76dd4321cf
ssun97 <sunmingyu.97@gmail.com>
2021-11-02 21:30:19 +0000

    format

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -5,5 +5,8 @@
 import typing
 
 
-def element_length(lst: typing.Iterable[typing.Sequence]) -> typing.List[typing.Tuple[typing.Sequence, int]]:
+def element_length(lst: typing.Iterable[typing.Sequence]) ->\
+                    typing.List[typing.Tuple[typing.Sequence, int]]:
+    """ Doc """
+
     return [(i, len(i)) for i in lst]
