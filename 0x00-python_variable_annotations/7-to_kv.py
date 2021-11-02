a82d16f2bb2f0b930f7cae204f066ccab145127e
ssun97 <sunmingyu.97@gmail.com>
2021-11-02 15:53:50 -0500

    Task 7

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" type-annotated function to_kv """
+
+
+import typing
+
+
+def to_kv(k: str, v: typing.Union[int, float]) -> typing.Tuple[str, float]:
+    """ Doc """
+    return [k, pow(v, 2)]
