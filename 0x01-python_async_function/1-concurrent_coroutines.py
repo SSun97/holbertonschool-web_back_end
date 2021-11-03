a6c8104270cc1734b3397065f9dc432f4a679151
ssun97 <sunmingyu.97@gmail.com>
2021-11-03 14:02:29 -0500

    task 1

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" Write an asynchronous coroutine that takes in 2 integer arguments  """
+
+import asyncio
+import random
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int = 10) -> list[float]:
+    """ Doc """
+    arr = []
+    arr1 = []
+    for x in range(n):
+        arr.append(wait_random(max_delay))
+    for item in asyncio.as_completed(arr):
+        arr1.append(await item)
+    return arr1
