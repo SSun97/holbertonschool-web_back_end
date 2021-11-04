797dfbb989716003d53dd95342a994c6f51a516c
ssun97 <sunmingyu.97@gmail.com>
2021-11-04 14:17:20 -0500

    task 2

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" write a measure_runtime coroutine that will execute async_comprehension four times """
+
+
+from typing import List
+import time
+import asyncio
+async_comprehension  = __import__('1-async_comprehension').async_comprehension 
+
+
+async def measure_runtime() -> float:
+    """ Doc """
+    start_time = time.time()
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+    )
+    end_time = time.time()
+    return (end_time - start_time)
