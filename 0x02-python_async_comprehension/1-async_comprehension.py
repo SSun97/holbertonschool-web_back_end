4e580253c2b18373d25c24cc0a10ee7bcf621acf
ssun97 <sunmingyu.97@gmail.com>
2021-11-04 13:33:20 -0500

    annotation

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -2,10 +2,10 @@
 """ write a coroutine called async_comprehension that takes no arguments """
 
 
-from typing import Generator
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[float, None, None]:
+async def async_comprehension() -> List[float]:
     """ Doc """
     return [i async for i in async_generator()]
