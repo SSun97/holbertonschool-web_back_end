4ec8aef37642e5dc53887d57b425451f417d5ff6
ssun97 <sunmingyu.97@gmail.com>
2021-11-04 14:20:04 -0500

    pycodestyle

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-""" write a measure_runtime coroutine that will execute async_comprehension four times """
+""" write a measure_runtime coroutine that will execute
+async_comprehension four times """
 
 
 from typing import List
 import time
 import asyncio
-async_comprehension  = __import__('1-async_comprehension').async_comprehension 
+async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
