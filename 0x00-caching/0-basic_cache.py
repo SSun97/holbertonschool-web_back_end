d38f1890b187636a9189e157ceb647896ca4c835
ssun97 <sunmingyu.97@gmail.com>
2021-11-09 13:13:33 -0600

    task 0

---
+++ 0x00-caching/0-basic_cache.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" Create a class BasicCache that inherits from BaseCaching
+and is a caching system """
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    def put(self, key, item):
+        """ Add an item in the cache """
+        self.cache_data[key] = item
+        # raise NotImplementedError("put must be implemented in your cache class")
+
+    def get(self, key):
+        """ Get an item by key """
+        if key is None or key is '' or key not in self.cache_data:
+            return None
+            # raise NotImplementedError(
+            #     "get must be implemented in your cache class")
+        return self.cache_data[key]
