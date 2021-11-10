206871061998be091e24c3232c929e261f14c4d7
ssun97 <sunmingyu.97@gmail.com>
2021-11-10 01:40:11 -0600

    pep8

--- 0x00-caching/0-basic_cache.py
+++ 0x00-caching/0-basic_cache.py
@@ -12,7 +12,6 @@ and is a caching system """
     def put(self, key, item):
         """ Add an item in the cache """
         self.cache_data[key] = item
-        # raise NotImplementedError("put must be implemented in your cache class")
 
     def get(self, key):
         """ Get an item by key """
