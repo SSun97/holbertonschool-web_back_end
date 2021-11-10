95ff7b6ee693d850f3b557abf742923378d43c40
ssun97 <sunmingyu.97@gmail.com>
2021-11-10 12:13:31 -0600

    Method put doesn’t do anything if key or item is None

--- 0x00-caching/1-fifo_cache.py
+++ 0x00-caching/1-fifo_cache.py
@@ -17,6 +17,8 @@ and is a caching system """
 
     def put(self, key, item):
         """ Add an item in the cache """
+        if key is None or item is None:
+            return
         self.cache_data[key] = item
         if key not in self.key_list:
             self.key_list.append(key)
@@ -24,7 +26,6 @@ and is a caching system """
             print("DISCARD: " + self.key_list[0])
             del self.cache_data[self.key_list[0]]
             del self.key_list[0]
-        # raise NotImplementedError("put must be implemented in your cache class")
 
     def get(self, key):
         """ Get an item by key """
