7c9cc544ad952458500f7a287274a15afae7968d
ssun97 <sunmingyu.97@gmail.com>
2021-11-10 13:04:27 -0600

    task 4

---
+++ 0x00-caching/4-mru_cache.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+""" Create a class MRUCache that inherits from BaseCaching
+and is a caching system """
+
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """ Create a class MRUCache that inherits from BaseCaching
+    and is a caching system """
+
+    def __init__(self):
+        """ init a object base on BaseCaching """
+        # self.cache_data = {}
+        super().__init__()
+        self.key_list = []
+
+    def put(self, key, item):
+        """ Add an item in the cache """
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+        if key in self.key_list:
+            self.key_list.remove(key)
+            self.key_list.insert(0, key)
+        if key not in self.key_list:
+            self.key_list.insert(1, key)
+            if len(self.key_list) > self.MAX_ITEMS:
+                print("DISCARD: " + self.key_list[0])
+                del self.cache_data[self.key_list[0]]
+                del self.key_list[0]
+
+    def get(self, key):
+        """ Get an item by key """
+        if key is None or key == '' or key not in self.cache_data:
+            return None
+        self.key_list.remove(key)
+        self.key_list.insert(0, key)
+        # raise NotImplementedError(
+        #     "get must be implemented in your cache class")
+        return self.cache_data[key]
