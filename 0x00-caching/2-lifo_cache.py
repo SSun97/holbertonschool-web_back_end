459641d1ca759cefc3749e6d7f5f6a75aa75dade
ssun97 <sunmingyu.97@gmail.com>
2021-11-10 12:16:14 -0600

    PEP8

--- 0x00-caching/2-lifo_cache.py
+++ 0x00-caching/2-lifo_cache.py
@@ -6,13 +6,19 @@ from base_caching import BaseCaching
 
 
 class LIFOCache(BaseCaching):
+    """ Create a class LIFOCache that inherits from BaseCaching
+    and is a caching system """
+
     def __init__(self):
+        """ init a object base on BaseCaching """
         # self.cache_data = {}
         super().__init__()
         self.key_list = []
 
     def put(self, key, item):
         """ Add an item in the cache """
+        if key is None or item is None:
+            return
         self.cache_data[key] = item
         if key in self.key_list:
             self.key_list.remove(key)
@@ -23,7 +29,6 @@ class LIFOCache(BaseCaching):
                 print("DISCARD: " + self.key_list[-2])
                 del self.cache_data[self.key_list[-2]]
                 del self.key_list[-2]
-        # raise NotImplementedError("put must be implemented in your cache class")
 
     def get(self, key):
         """ Get an item by key """
