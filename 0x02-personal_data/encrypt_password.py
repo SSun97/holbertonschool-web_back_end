4a4c693c098344b3a22b6e6e50884e36e5557e49
ssun97 <sunmingyu.97@gmail.com>
2021-11-17 16:09:01 -0600

    task 6

--- 0x02-personal_data/encrypt_password.py
+++ 0x02-personal_data/encrypt_password.py
@@ -8,3 +8,8 @@ def hash_password(password: str) -> bytes:
     a salted, hashed password, which is a byte string"""
     binary_password = bytes(password, "ascii")
     return bcrypt.hashpw(binary_password, bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ expects 2 arguments and returns a boolean """
+    return bcrypt.checkpw(bytes(password, "ascii"), hashed_password)
