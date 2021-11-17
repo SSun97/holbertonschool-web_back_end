830230b4254ff60216c64a98769a5fd25a8e6d7f
ssun97 <sunmingyu.97@gmail.com>
2021-11-17 15:35:09 -0600

    Task 4

--- 0x02-personal_data/filtered_logger.py
+++ 0x02-personal_data/filtered_logger.py
@@ -60,3 +60,30 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
         database=os.environ.get('PERSONAL_DATA_DB_NAME', 'root'),
         user=os.environ.get('PERSONAL_DATA_DB_USERNAME'),
         password=os.environ.get('PERSONAL_DATA_DB_PASSWORD', ''))
+
+
+def main():
+    """ The function will obtain a database connection using get_db and
+    retrieve all rows in the users table and display each row under a
+    filtered format """
+    dbObj = get_db()
+    cursor = dbObj.cursor()
+    cursor.execute("SELECT * FROM users;")
+    result = cursor.fetchall()
+    for row in result:
+        message = f"name={row[0]}; " + \
+                  f"email={row[1]}; " + \
+                  f"phone={row[2]}; " + \
+                  f"ssn={row[3]}; " + \
+                  f"password={row[4]};"
+        print(message)
+        logger = logging.LogRecord("my_logger", logging.INFO,
+                                   None, None, message, None, None)
+        formatter = RedactingFormatter(PII_FIELDS)
+        formatter.format(logger)
+    cursor.close()
+    dbObj.close()
+
+
+if __name__ == "__main__":
+    main()
