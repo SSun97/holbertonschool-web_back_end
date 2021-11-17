2d4fcdf622eae094424c7b1618e5550243739ebe
ssun97 <sunmingyu.97@gmail.com>
2021-11-17 16:04:45 -0600

    create encrypted password

--- 0x02-personal_data/encrypt_password.py
+++ 0x02-personal_data/encrypt_password.py
@@ -1,2 +1,10 @@
 #!/usr/bin/env python3
 """ User passwords should NEVER be stored in plain text in a database """
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """ function that expects one string argument and returns
+    a salted, hashed password, which is a byte string"""
+    binary_password = bytes(password, "ascii")
+    return bcrypt.hashpw(binary_password, bcrypt.gensalt())
