d1a876cafd15bc4d30ef9324573b5820712d4b50
ssun97 <sunmingyu.97@gmail.com>
2021-11-21 17:12:52 -0600

    pep8

--- 0x03-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x03-Basic_authentication/api/v1/auth/basic_auth.py
@@ -88,5 +88,5 @@ class BasicAuth(Auth):
         decodeValue = self.decode_base64_authorization_header(base64Header)
         credentials = self.extract_user_credentials(decodeValue)
         user = self.user_object_from_credentials(credentials[0],
-                                                    credentials[1])
+                                                 credentials[1])
         return user
