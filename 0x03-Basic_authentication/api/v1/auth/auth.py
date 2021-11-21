3fbfed5316d6b9b3b730d00eedbe805c5e8db361
ssun97 <sunmingyu.97@gmail.com>
2021-11-20 20:30:20 -0600

    Task 5

--- 0x03-Basic_authentication/api/v1/auth/auth.py
+++ 0x03-Basic_authentication/api/v1/auth/auth.py
@@ -20,7 +20,11 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """ Public method return None """
-        return None
+        if request is None:
+            return None
+        if not request.headers.get("Authorization"):
+            return None
+        return request.headers.get("Authorization")
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ Public method returns None """
