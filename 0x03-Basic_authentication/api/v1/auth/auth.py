0a80f5405b7df9db9cd0351351c919e5817fabd9
ssun97 <sunmingyu.97@gmail.com>
2021-11-20 18:22:34 -0600

    modify

--- 0x03-Basic_authentication/api/v1/auth/auth.py
+++ 0x03-Basic_authentication/api/v1/auth/auth.py
@@ -1,13 +1,17 @@
+#!/usr/bin/env python3
+""" auth file """
 from flask import request
 from typing import List, TypeVar
 
+
 class Auth:
     """ class for authorization """
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Public method return False """
         return False
 
-    def authorization_header(self, request=None) -> str: 
+    def authorization_header(self, request=None) -> str:
         """ Public method return None """
         return None
 
