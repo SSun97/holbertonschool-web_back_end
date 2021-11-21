a365e3017cae38c268ef9297ef12a283659f7ba3
ssun97 <sunmingyu.97@gmail.com>
2021-11-21 16:56:14 -0600

    Task 10

--- 0x03-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x03-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """ Create a class BasicAuth that inherits from Auth """
 
-from typing import Tuple
+from typing import Tuple, TypeVar
 from api.v1.auth.auth import Auth
 import re
 from base64 import b64decode
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -63,3 +64,18 @@ class BasicAuth(Auth):
             return None, None
         credentials = decoded_base64_authorization_header.split(':')
         return credentials[0], credentials[1]
+
+    def user_object_from_credentials(self, user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        """ User Object """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+        try:
+            users = User.search({"email": user_email})
+            for user in users:
+                if user.is_valid_password(user_pwd):
+                    return user
+        except Exception:
+            None
