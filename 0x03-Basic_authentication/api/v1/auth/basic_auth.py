e802707f92c226d42620729c6eefda37fce74e08
ssun97 <sunmingyu.97@gmail.com>
2021-11-21 16:07:25 -0600

    task 6

---
+++ 0x03-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Create a class BasicAuth that inherits from Auth """
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """ Basic Auth method """
+    pass
