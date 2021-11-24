2196ac25fcf38095b0b540e41a319d0f15223aab
ssun97 <sunmingyu.97@gmail.com>
2021-11-24 13:28:50 -0600

    task 1

---
+++ 0x04-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+SessionAuth module for the API
+"""
+from api.v1.auth.auth import Auth
+from models.user import User
+import uuid
+
+
+class SessionAuth(Auth):
+    """ SessionAuth
+    """
+    pass
