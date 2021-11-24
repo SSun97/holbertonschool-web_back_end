3816028ac3528c4865959eee2d46be2e300ca036
ssun97 <sunmingyu.97@gmail.com>
2021-11-24 14:53:33 -0600

    task 8

--- 0x04-Session_authentication/api/v1/auth/session_auth.py
+++ 0x04-Session_authentication/api/v1/auth/session_auth.py
@@ -28,3 +28,32 @@ class SessionAuth(Auth):
         if type(session_id) is not str:
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """ Current user
+            (overload) that returns a User instance based on a cookie value
+        """
+        session_id = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_id)
+
+        try:
+            return User.get(id=user_id)
+        except KeyError:
+            return None
+
+    def destroy_session(self, request=None):
+        """ destroy session
+        """
+        if request is None:
+            return False
+
+        session_id = self.session_cookie(request)
+
+        if not session_id:
+            return False
+
+        if not self.user_id_for_session_id(session_id):
+            return False
+
+        del self.user_id_by_session_id[session_id]
+        return True
--- 0x04-Session_authentication/api/v1/views/session_auth.py
+++ 0x04-Session_authentication/api/v1/views/session_auth.py
@@ -39,3 +39,17 @@ def login() -> str:
             response.set_cookie(getenv('SESSION_NAME'), session_id)
             return response
     return jsonify({"error": "wrong password"}), 401
+
+@app_views.route('/auth_session/logout', methods=['DELETE'],
+                 strict_slashes=False)
+def logout() -> str:
+    """ DELETE /api/v1/auth_session/logout
+    Return:
+      - Logout User authenticated
+    """
+
+    from api.v1.app import auth
+    if auth.destroy_session(request):
+        return jsonify({}), 200
+    else:
+        abort(404)
