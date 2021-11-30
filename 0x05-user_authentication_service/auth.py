60035f144af7fbe997fd824a5cddeece1070e549
ssun97 <sunmingyu.97@gmail.com>
2021-11-30 15:03:27 -0600

    Task 12

--- 0x05-user_authentication_service/auth.py
+++ 0x05-user_authentication_service/auth.py
@@ -2,6 +2,7 @@
 """ define a _hash_password method that takes in a password string arguments
 and returns bytes. """
 
+from re import S
 import bcrypt
 from sqlalchemy.orm import exc
 from db import DB
@@ -56,5 +57,15 @@ class Auth:
             session_id = _generate_uuid()
             self._db.update_user(user.id, session_id=session_id)
             return session_id
+        except NoResultFound:
+            return None
+
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """ get user from session id """
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
         except NoResultFound:
             return None
