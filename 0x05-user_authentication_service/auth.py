68288a4bd015a74978ea1478479721b7469d2ab8
ssun97 <sunmingyu.97@gmail.com>
2021-11-30 12:50:01 -0600

    function is correctly annotated

--- 0x05-user_authentication_service/auth.py
+++ 0x05-user_authentication_service/auth.py
@@ -22,7 +22,7 @@ class Auth:
     def __init__(self):
         self._db = DB()
 
-    def register_user(self, email, password) -> User:
+    def register_user(self, email: str, password: str) -> User:
         """ register user """
         try:
             self._db.find_user_by(email=email)
