0916607fa69d57ead9ed2d92189826fec1671907
ssun97 <sunmingyu.97@gmail.com>
2021-11-30 17:24:17 -0600

    everything should be doced

--- 0x05-user_authentication_service/app.py
+++ 0x05-user_authentication_service/app.py
@@ -93,6 +93,7 @@ def get_reset_password_token() -> str:
 
 @app.route('/reset_password', methods=['PUT'])
 def update_password() -> str:
+    """ update password if reset token exist """
     email = request.form.get('email')
     reset_token = request.form.get('reset_token')
     new_password = request.form.get('new_password')
