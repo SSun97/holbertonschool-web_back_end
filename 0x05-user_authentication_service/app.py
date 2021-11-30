b9a28b320338c81ca928b7f533d49f37eab4895d
ssun97 <sunmingyu.97@gmail.com>
2021-11-30 14:57:05 -0600

    finalize

--- 0x05-user_authentication_service/app.py
+++ 0x05-user_authentication_service/app.py
@@ -42,7 +42,7 @@ def login() -> str:
     if not email or not password or not AUTH.valid_login(email, password):
         abort(401)
 
-    session_id = Auth.create_session(email)
+    session_id = AUTH.create_session(email)
     response = jsonify({"email": email, "message": "logged in"})
     response.set_cookie("session_id", session_id)
     return response
