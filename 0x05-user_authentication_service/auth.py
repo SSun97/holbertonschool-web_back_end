fb21cb1e50807ce283e74e912b4f16b47b5ef11f
ssun97 <sunmingyu.97@gmail.com>
2021-11-30 15:35:19 -0600

    update destroy session

--- 0x05-user_authentication_service/auth.py
+++ 0x05-user_authentication_service/auth.py
@@ -73,4 +73,4 @@ class Auth:
     def destroy_session(self, user_id: int) -> None:
         """ destroy session by user id """
         self._db.update_user(user_id, session_id=None)
-        return None
+
