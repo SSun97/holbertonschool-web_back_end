c429f4fb55d4d6d6a4bde91d24dcfa9a0838e56a
ssun97 <sunmingyu.97@gmail.com>
2021-12-12 10:37:38 -0600

    pep8

--- 0x01-i18n/2-app.py
+++ 0x01-i18n/2-app.py
@@ -1,21 +1,29 @@
+#!/usr/bin/env python3
 from flask import Flask, render_template, request
 from flask_babel import Babel, gettext as _
 
 app = Flask(__name__)
 babel = Babel(app)
+
+
 class Config(object):
     LANGUAGE = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
 app.config.from_object(Config)
+
 
 @app.route('/')
 def hello():
     return render_template('2-index.html')
 
+
 @babel.localeselector
 def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGE'])
+
 
 if __name__ == '__main__':
     app.debug = True
--- 0x01-i18n/4-app.py
+++ 0x01-i18n/4-app.py
@@ -1,17 +1,24 @@
+#!/usr/bin/env python3
 from flask import Flask, render_template, request
 from flask_babel import Babel, gettext as _
 
 app = Flask(__name__)
 babel = Babel(app)
+
+
 class Config(object):
     LANGUAGE = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
 app.config.from_object(Config)
+
 
 @app.route('/')
 def hello():
     return render_template('4-index.html')
+
 
 @babel.localeselector
 def get_locale():
@@ -20,6 +27,7 @@ def get_locale():
         return local_lan
     else:
         return request.accept_languages.best_match(app.config['LANGUAGE'])
+
 
 if __name__ == '__main__':
     app.debug = True
