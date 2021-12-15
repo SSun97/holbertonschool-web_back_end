8645c5099106f127758af4fc7fac2b3aaba012c3
ssun97 <sunmingyu.97@gmail.com>
2021-12-15 16:16:33 -0600

    task 0

---
+++ 0x01-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,4 @@
+-- Write a SQL script that creates a table users following these requirements:
+-- Make an attribute unique directly in the table schema will enforced your
+-- business rules and avoid bugs in your application
+CREATE TABLE USER IF NOT EXISTS;
