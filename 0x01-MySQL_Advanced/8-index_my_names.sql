0421cd6b3dd9e2fa43a724c7527acba7be8cb62a
ssun97 <sunmingyu.97@gmail.com>
2021-12-16 15:38:22 -0600

    rename

--- 0x01-MySQL_Advanced/8-index_my_names.sql
+++ 0x01-MySQL_Advanced/8-index_my_names.sql
@@ -1,3 +1,3 @@
 -- Write a SQL script that creates an index idx_name_first on the table names and the first letter of name
-CREATE INDEX name_idx
+CREATE INDEX idx_name_first
     ON names (name(1));
