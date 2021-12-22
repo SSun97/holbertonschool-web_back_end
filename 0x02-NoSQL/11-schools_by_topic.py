b813c0c6c151c1f77d47bd8781616a2368e16781
ssun97 <sunmingyu.97@gmail.com>
2021-12-21 18:34:01 -0600

    update return

--- 0x02-NoSQL/11-schools_by_topic.py
+++ 0x02-NoSQL/11-schools_by_topic.py
@@ -5,4 +5,4 @@ list of school having a specific topic: """
 
 def schools_by_topic(mongo_collection, topic):
     """Returns a list of dictionaries that return a topic"""
-    return [doc for doc in mongo_collection.find({'topic': topic})]
+    return mongo_collection.find({ "topics": topic })
