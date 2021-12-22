2de242a7961569008a286cc80ec3d6b08f83389c
ssun97 <sunmingyu.97@gmail.com>
2021-12-21 18:43:08 -0600

    creat list to store method's name

--- 0x02-NoSQL/12-log_stats.py
+++ 0x02-NoSQL/12-log_stats.py
@@ -3,24 +3,14 @@
 from pymongo import MongoClient
 
 
-def helper(a: dict) -> int:
-    """return log"""
+if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     logs = client.logs.nginx
-    return logs.count_documents(a)
 
-
-def main():
-    """ provides some stats about Nginx logs stored in MongoDB """
-    print(f"{helper({})} logs")
-    print("Methods:")
-    print(f"\tmethod GET: {helper({'method': 'GET'})}")
-    print(f"\tmethod POST: {helper({'method': 'POST'})}")
-    print(f"\tmethod PUT: {helper({'method': 'PUT'})}")
-    print(f"\tmethod PATCH: {helper({'method': 'PATCH'})}")
-    print(f"\tmethod DELETE: {helper({'method': 'DELETE'})}")
-    print(f"{helper({'method': 'GET', 'path': '/status'})} status check")
-
-
-if __name__ == "__main__":
-    main()
+    print(logs.count_documents({}), 'logs')
+    print('Methods:')
+    method = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    for req in method:
+        print(f'\tmethod {req}:', logs.count_documents({'method': req}))
+    print(logs.count_documents(
+        {'method': 'GET', 'path': '/status'}), 'status check')
