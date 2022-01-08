ead6cb34b30499e732960b35810a715b3464e6a6
ssun97 <sunmingyu.97@gmail.com>
2022-01-07 18:21:59 -0600

    test

--- 0x00-Node_JS_basic/5-http.js
+++ 0x00-Node_JS_basic/5-http.js
@@ -1,20 +1,24 @@
 const http = require('http');
 const countStudents = require('./3-read_file_async');
 
-const app = http.createServer(async (req, res) => {
+const pathToCSVFile = process.argv[2];
+
+const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  if (req.url === '/') res.write('Hello Holberton School!');
+
+  // Endpoints
   if (req.url === '/students') {
-    res.write('This is the list of our students\n');
-    try {
-      const data = await countStudents(process.argv[2]);
-      res.end(`${data.join('\n')}`);
-    } catch (error) {
-      res.end(error.message);
-    }
+    countStudents(pathToCSVFile, 0)
+      .then((success) => {
+        const out = `This is the list of our students\n${success}`;
+        res.end(out);
+      });
+  } else {
+    res.end('Hello Holberton School!');
   }
-  res.end();
 });
+
 app.listen(1245);
+
 module.exports = app;
