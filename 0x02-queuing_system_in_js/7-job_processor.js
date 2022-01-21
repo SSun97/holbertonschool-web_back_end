67c1b7390760e3aae4d5e58f9e2019d5b219199a
ssun97 <sunmingyu.97@gmail.com>
2022-01-20 22:15:47 -0600

    Task 8-10

--- 0x02-queuing_system_in_js/7-job_processor.js
+++ 0x02-queuing_system_in_js/7-job_processor.js
@@ -7,11 +7,12 @@ const maxProcessesConcurrent = 2;
 const sendNotification = (phoneNumber, message, job, done) => {
   if (blaklistedPhoneNumbers.includes(phoneNumber)) done(new Error(`Phone number ${phoneNumber} is blacklisted`));
  
-  job.progress(0, 50, 100);
+  job.progress(50, 100);
   console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
   done();
 };
 
 queue.process('push_notification_code_2', maxProcessesConcurrent, (job, done) => {
   sendNotification(job.data.phoneNumber, job.data.message, job, done);
+  done();
 });
